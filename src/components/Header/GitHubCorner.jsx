5e281fddd7857d3222686b24f1a20b15270a2bb7
Tarik <iot.ai.master@gmail.com>
2026-04-17 11:01:27 -0400

    temp corner

---
+++ src/components/Header/GitHubCorner.jsx
@@ -0,0 +1,18 @@
+export default function GitHubCorner({ styles }) {
+  return (
+    <a
+      href="https://github.com/tholman/github-corners"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <svg
+        className={styles.logo}
+        viewBox="0 0 250 250"
+        width="80"
+        height="80"
+      >
+        https://github.com/tholman/github-corners/blame/master/svg/github-corner-left.svg
+      </svg>
+    </a>
+  );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import {themes} from "../../themes/themes";
 import styles from "./Header.module.css";
-
+import GitHubCorner from "./GitHubCorner";
 
 
 export default function Header() {
@@ -13,8 +13,11 @@ export default function Header() {
 
     return (
         <div>
+            <GitHubCorner styles={styles} />
             <HeaderThemeSwitcher />
-            <h1 className={styles.headerStyle} style={{color: themes[theme].color}}>Just do it.</h1>
+            <div className={styles.titleWrapper}>
+                <h1 className={styles.headerStyle} style={{color: themes[theme].color}}>Just do it.</h1>
+            </div>
         </div>
     );
 }
